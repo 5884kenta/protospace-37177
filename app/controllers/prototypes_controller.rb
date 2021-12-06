b007567dc13d0d39d05502e4a6a080df37daf262
5884kenta <babolat.i.kamo@gmail.com>
2021-12-06 16:13:30 +0900

    新規登録修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -12,6 +12,7 @@ class PrototypesController < ApplicationController
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path      
     else
